cf4e84bef68f082dc12f741eed4610de18f37c78
KAMoviniMudara <mudaramovini@gmail.com>
2023-07-25 14:08:15 +0530

    Commit Repository

---
+++ src/main/java/com/example/CustomerManag/repository/ItemRepository.java
@@ -0,0 +1,9 @@
+package com.example.CustomerManag.repository;
+
+import com.example.CustomerManag.entity.Item;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ItemRepository extends CrudRepository<Item, Long> {
+}
---
+++ src/main/java/com/example/CustomerManag/repository/OrderRepository.java
@@ -0,0 +1,9 @@
+package com.example.CustomerManag.repository;
+
+import com.example.CustomerManag.entity.Order;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface OrderRepository extends CrudRepository<Order, Long> {
+}
